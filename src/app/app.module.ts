8eb44e7bb9045fead0bd2b17856ba25c53a111f2
zakariatoumi <zakariatoumi94@gmail.com>
2019-12-02 21:16:04 +0100

    Administration de projet MovieFilms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { AddFilmComponent } from './films/add-film/add-film.component';
+import { ListFilmsComponent } from './films/list-films/list-films.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+//import { FormsModule } from '@angular/forms';
+
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'AjouterFilm', component: AddFilmComponent },
+  { path: 'listFilms', component: ListFilmsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'Register', component: RegisterComponent },
+
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddFilmComponent,
+    ListFilmsComponent,
+    LoginComponent,
+    RegisterComponent,
+    //FormsModule,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/films/list-films/list-films.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-films',
+  templateUrl: './list-films.component.html',
+  styleUrls: ['./list-films.component.css']
+})
+export class ListFilmsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
